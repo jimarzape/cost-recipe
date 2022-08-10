380f68610a6b9085a96a1cf4f13a1582bd64f795
unknown <jimar@bluehive.com.ph>
2022-08-10 09:46:06 +0800

    update add srp column

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -12,7 +12,8 @@
                     <th>Recipe Name</th>
                     <th>Servings</th>
                     <th width="300">Costs</th>
-                    <th>SRP (50%)</th>
+                    <th>SRP</th>
+                    <th>Net</th>
                     <th class="text-center">Action</th>
                 </tr>
             </thead>
@@ -23,6 +24,7 @@
                     <td class="text-right">{{number_format($recipe->serving_count)}}</td>
                     <td class="text-right"><span class="f-left">₱</span>{{number_format($recipe->total_cost, 2)}}</td>
                     <td class="text-right"><span class="f-left">₱</span>{{number_format($recipe->srp, 2)}}</td>
+                    <td class="text-right"><span class="f-left">₱</span>{{number_format(($recipe->srp - $recipe->total_cost), 2)}}</td>
                     <td class="text-center">
                         <a class="btn btn-primary btn-sm" href="{{route('recipe.view', $recipe->id)}}"><i class="fa fa-folder-open-o"></i></a>
                         <a class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></a>
